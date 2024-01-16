1f466b5c65ba55acf0c368eb8e355feb1bb1067b
Krabs <vovastr0v@yandex.ru>
2024-01-17 01:58:59 +0500

    script - currency api

--- scripts/url_demand.py
+++ scripts/url_demand.py
@@ -5,6 +5,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 import json
+from utils import get_currencies, change_data_format
 
 
 def get_medium(x):
@@ -14,9 +15,7 @@ def get_medium(x):
 
 
 def get_year_vacancy(s):
-    for j in re.findall(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4}', s):
-        s = s.replace(j, j[0:4])
-    return int(s)
+    return int(re.findall(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4}', s)[0][0:4])
 
 
 def get_vac_by_years(data):
@@ -49,14 +48,38 @@ def create_report():
     return data
 
 
+def to_rub(salary, published_at, currency, hash_dict):
+    date = change_data_format(published_at)
+    medium_salary = salary
+    if pd.isna(currency):
+        return medium_salary
+    if currency == 'RUR':
+        return medium_salary
+    if date in hash_dict.keys():
+        if currency not in hash_dict[date].keys():
+            return medium_salary
+        return medium_salary * hash_dict[date][currency]
+    print(published_at)
+    currencies = get_currencies(date)
+    hash_dict[date] = currencies
+    if currency in currencies.keys():
+        return medium_salary * currencies[currency]
+    return medium_salary
+
+
 def parse_csv(csv):
-    names = ['name', 'salary_from', 'salary_to', 'salary_currency', 'area_name', 'published_at']
+    hash_currencies = dict()
+    names = ['Index', 'name', 'salary_from', 'salary_to', 'salary_currency', 'area_name', 'published_at']
     vacancies = (pd.read_csv(csv, names=names, low_memory=False)
                  .assign(salary_from=lambda x: x['salary_from'].fillna(x['salary_to']))
                  .assign(salary_to=lambda x: x['salary_to'].fillna(x['salary_from']))
-                 .assign(published_at=lambda x: x['published_at'].apply(get_year_vacancy))
                  .assign(count=0)
                  .assign(medium_salary=get_medium))
+    vacancies['Index'] = range(0, len(vacancies))
+    vacancies.set_index('Index', inplace=True)
+    vacancies['medium_salary'] = vacancies.apply(lambda x: to_rub(
+        x['medium_salary'], x['published_at'], x['salary_currency'], hash_currencies), axis=1)
+    vacancies['published_at'] = vacancies['published_at'].apply(get_year_vacancy)
     return vacancies
 
 
@@ -65,7 +88,7 @@ def create_plot():
     vac = 'engineer|инженер программист|інженер|it инженер|инженер разработчик'
     vacancies = parse_csv(csv)
     data = []
-    fig, sub = plt.subplots(2, 1, figsize=(8, 10))
+    fig, sub = plt.subplots(3, 1, figsize=(10, 15))
     vac_by_years = get_vac_by_years(vacancies)['medium_salary'].to_dict()
     vac_by_years_filtered = get_vac_by_years(vacancies[vacancies['name']
                                              .str.contains(vac, na=False, case=False)])['medium_salary'].to_dict()
@@ -102,17 +125,30 @@ def create_plot():
     xlable = range(min(vac_by_years_filtered.keys()), max(vac_by_years_filtered.keys()) + 1)
     x = np.arange(len(xlable))
     y = [i for i in vac_by_years.values()]
-    y2 = [i for i in vac_by_years_filtered.values()]
-    ax.bar(x - width / 2, y, width, label='Количество вакансий')
-    ax.bar(x + width / 2, y2, width, label=f'Количество вакансий инженер программист')
+
+    ax.bar(x, y, width)
 
     ax.set_title('Количество вакансий по годам')
     ax.set_xticks(x)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha='right')
     ax.set_xticklabels(xlable)
     set_font_size(ax, 8)
-    ax.legend(fontsize=12)
+
     ax.grid(axis='y')
+
+    width = 0.3
+    ax = sub[2]
+    xlable = range(min(vac_by_years_filtered.keys()), max(vac_by_years_filtered.keys()) + 1)
+    x = np.arange(len(xlable))
+    y2 = [i for i in vac_by_years_filtered.values()]
+    ax.bar(x, y2, width)
+    ax.set_title('Количество вакансий по годам для профессии инженер-программист')
+    ax.set_xticks(x)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90, ha='right')
+    ax.set_xticklabels(xlable)
+    set_font_size(ax, 8)
+    ax.grid(axis='y')
+
     return fig, sub, data
 
 
@@ -151,13 +187,13 @@ def create_pdf():
         file.write(json.dumps(dict(zip(*data_year_keys))))
         file.write('\n')
         print("Динамика уровня зарплат по годам для профессии инженер-программист:")
-        file.write(json.dumps(dict(zip(*data_year_values_filtered))))
+        file.write(json.dumps(dict(zip(*data_year_keys_filtered))))
         file.write('\n')
         print("Динамика количества вакансий по годам:")
         file.write(json.dumps(dict(zip(*data_year_values))))
         file.write('\n')
         print("Динамика количества вакансий по годам для профессии инженер-программист:")
-        file.write(json.dumps(dict(zip(*data_year_keys_filtered))))
+        file.write(json.dumps(dict(zip(*data_year_values_filtered))))
         file.write('\n')
 
 
---
+++ scripts/utils.py
@@ -0,0 +1,19 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def change_data_format(s):
+    a = re.findall(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{4}', s)[0][:10]
+    m = a[-5:-3]
+    y = a[:4]
+    return m + '/' + y
+
+
+def get_currencies(date):
+    base_url = f'https://www.cbr.ru/scripts/XML_daily.asp?date_req=01/{date}'
+    response = requests.get(base_url).content
+    x = BeautifulSoup(response, "xml")
+    valutes = [(i.find('CharCode').text, float(i.find('Value').text.replace(',', '.')))
+               for i in x.select('ValCurs > Valute')]
+    return dict(valutes)
