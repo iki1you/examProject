53e89e46fe67ad0c7b126383a3d9bb2fa6a1d932
Krabs <vovastr0v@yandex.ru>
2024-01-20 19:10:41 +0500

    final project

--- scripts/hhapi.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-BASE_URL = ("https://api.hh.ru/vacancies?search_field=name&text=':(инженер AND "
-            "программист)'&order_by=publication_time&per_page=10")
-response = requests.get(BASE_URL)
-vacancies = response.json()
-for i in vacancies['items']:
-    print(i['name'], i['published_at'])
